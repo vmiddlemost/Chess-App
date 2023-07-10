741e7dc8032146f9bda7c8cd0d7ccbe91266889d
vmiddlemost <68695177+vmiddlemost@users.noreply.github.com>
2023-07-10 14:34:18 +0100

    Update 10/07/2023
    Added the movement and attacking rules for the rest of the pieces (Rooks, Knights, Bishops, Queens and Kings).

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -92,7 +92,8 @@ export default class Referee
                 {
                     return true;
                 }
-            } else if (initialPosition.x === desiredPosition.x 
+            }
+            else if (initialPosition.x === desiredPosition.x 
                 && desiredPosition.y - initialPosition.y === pawnDirection) 
             {
                 if (!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState))
@@ -120,17 +121,17 @@ export default class Referee
                 }
             }
         } 
-        // ROOK RULES
-        if (type === PieceType.ROOK)
+        // ROOK/QUEEN STRAIGHT RULES
+        if (type === PieceType.ROOK
+            || type === PieceType.QUEEN)
         {
-            // MOVEMENT LOGIC
-            // VERICAL MOVEMENT
+            // VERTICAL MOVEMENT LOGIC
             if (initialPosition.x === desiredPosition.x 
                 && initialPosition.y !== desiredPosition.y)
             {
                 let direction = (desiredPosition.y < initialPosition.y) ? -1 : 1;
 
-                for (let i = 1; i <= Math.abs(desiredPosition.y - initialPosition.y); i++)
+                for (let i = 1; i <= Math.abs(desiredPosition.y - initialPosition.y) - 1; i++)
                 {
                     if (this.tileIsOccupied(desiredPosition.x, initialPosition.y + (i * direction), boardState))
                     {
@@ -141,15 +142,21 @@ export default class Referee
                 {
                     return true;
                 }
-            } else if (initialPosition.y === desiredPosition.y 
+                // VERTICAL ATTACKING LOGIC
+                else if (this.tileIsOccupiedByOpponent(desiredPosition.x, desiredPosition.y, boardState, team))
+                {
+                    return true;
+                }
+            }
+            else if (initialPosition.y === desiredPosition.y 
                 && initialPosition.x !== desiredPosition.x)
-                // HORIZONTAL MOVEMENT
+                // HORIZONTAL MOVEMENT LOGIC
             {
                 let direction = (desiredPosition.x < initialPosition.x) ? -1 : 1;
 
-                for (let i = 1; i <= Math.abs(desiredPosition.y - initialPosition.y); i++)
+                for (let i = 1; i <= Math.abs(desiredPosition.x - initialPosition.x) - 1; i++)
                 {
-                    if (this.tileIsOccupied(initialPosition.x + (i * direction), initialPosition.y, boardState))
+                    if (this.tileIsOccupied(initialPosition.x + (i * direction), desiredPosition.y, boardState))
                     {
                         return false;
                     } 
@@ -158,8 +165,102 @@ export default class Referee
                 {
                     return true;
                 }
+                // HORIZONTAL ATTACKING LOGIC
+                else if (this.tileIsOccupiedByOpponent(desiredPosition.x, desiredPosition.y, boardState, team))
+                {
+                    return true;
+                }
             }
         }
+        // KNIGHT RULES
+        if (type === PieceType.KNIGHT)
+        {
+            // MOVEMENT LOGIC
+            if ((Math.abs(desiredPosition.x - initialPosition.x) === 1 && Math.abs(desiredPosition.y - initialPosition.y) === 2)
+            || (Math.abs(desiredPosition.x - initialPosition.x) === 2 && Math.abs(desiredPosition.y - initialPosition.y) === 1))
+            {
+                // ATTACKING LOGIC
+                if (this.tileIsOccupiedByOpponent(desiredPosition.x, desiredPosition.y, boardState, team))
+                {
+                    return true;
+                }
+                else if (this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+        // BISHOP/QUEEN DIAGONAL RULES
+        if (type === PieceType.BISHOP
+            || type === PieceType.QUEEN)
+            {
+                // MOVEMENT RULES (\ DIRECTION)
+                if (initialPosition.x + initialPosition.y === desiredPosition.x + desiredPosition.y)
+                {
+                    let direction = (desiredPosition.y < initialPosition.y) ? -1 : 1;
+
+                    for (let i = 1; i <= Math.abs(desiredPosition.y - initialPosition.y) - 1; i++)
+                    {
+                        if (this.tileIsOccupied(initialPosition.x - (i * direction), initialPosition.y + (i * direction), boardState))
+                        {
+                            return false;
+                        } 
+                    }
+                    if (!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState))
+                    {
+                        return true;
+                    }
+                    // ATTACKING LOGIC (\ DIRECTION)
+                    else if (this.tileIsOccupiedByOpponent(desiredPosition.x, desiredPosition.y, boardState, team))
+                    {
+                        return true;
+                    }
+                }
+                // MOVEMENT RULES (/ DIRECTION)
+                else if (initialPosition.x - desiredPosition.x === initialPosition.y - desiredPosition.y)
+                {
+                    let direction = (desiredPosition.y < initialPosition.y) ? -1 : 1;
+
+                    for (let i = 1; i <= Math.abs(desiredPosition.y - initialPosition.y) - 1; i++)
+                    {
+                        if (this.tileIsOccupied(initialPosition.x + (i * direction), initialPosition.y + (i * direction), boardState))
+                        {
+                            return false;
+                        } 
+                    }
+                    if (!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState))
+                    {
+                        return true;
+                    }
+                    // ATTACKING LOGIC (/ DIRECTION)
+                    else if (this.tileIsOccupiedByOpponent(desiredPosition.x, desiredPosition.y, boardState, team))
+                    {
+                        return true;
+                    }
+                }
+            }
+            // KING RULES
+            if (type === PieceType.KING)
+            {
+                if ((Math.abs(initialPosition.x - desiredPosition.x) === 0 && Math.abs(initialPosition.y - desiredPosition.y) === 1)
+                || (Math.abs(initialPosition.x - desiredPosition.x) === 1 && Math.abs(initialPosition.y - desiredPosition.y) === 0)
+                || (Math.abs(initialPosition.x - desiredPosition.x) === 1 && Math.abs(initialPosition.y - desiredPosition.y) === 1))
+                {
+                    if (!this.tileIsOccupied(desiredPosition.x, desiredPosition.y, boardState))
+                    {
+                        return true;
+                    }
+                    // ATTACKING LOGIC
+                    else if (this.tileIsOccupiedByOpponent(desiredPosition.x, desiredPosition.y, boardState, team))
+                    {
+                        return true;
+                    }
+                }
+            }
 
         return false;
     }
